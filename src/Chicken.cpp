b88559a561c20a0e8a7088f0b65d0e245199800c
Songkeys <zy22132@nottingham.edu.cn>
2019-04-04 01:59:56 +0100

    Add chicken png

--- src/Chicken.cpp
+++ src/Chicken.cpp
@@ -1,14 +1,32 @@
 #include "header.h"
 #include "Chicken.h"
-
-
+#include "GameEngine.h"
 
 Chicken::Chicken(int x, int y, GameEngine* pEngine, int w, int h)
-	: DisplayableObject(x, y, (BaseEngine*) pEngine, w, h, true)
+	: DisplayableObject(x, y, (BaseEngine*) pEngine, w, h, true),
+	image(pEngine->loadImage("resources/images/chicken.png", false))
 {
+	m_iDrawWidth = w;
+	m_iDrawHeight = h;
+	m_iStartDrawPosX = 0;
+	m_iStartDrawPosY = 0;
 }
 
 
 Chicken::~Chicken()
 {
+}
+
+void Chicken::virtDraw()
+{
+	if (isVisible())
+	{
+		image.renderImage(getEngine()->getForegroundSurface(),
+			0,
+			0,
+			m_iCurrentScreenX + m_iStartDrawPosX,
+			m_iCurrentScreenY + m_iStartDrawPosY,
+			m_iDrawWidth,
+			m_iDrawHeight);
+	}
 }
--- src/Chicken.h
+++ src/Chicken.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "DisplayableObject.h"
+#include "ImageManager.h"
 class GameEngine;
 class Chicken :
 	public DisplayableObject
@@ -7,5 +8,8 @@ class Chicken :
 public:
 	Chicken(int x, int y, GameEngine* pEngine, int w, int h);
 	~Chicken();
+	virtual void virtDraw() override;
+private:
+	SimpleImage image;
 };
 
