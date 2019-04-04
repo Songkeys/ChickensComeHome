7b8044427858549fab795e7b8b4afa6a4e3fcfc2
Songkeys <zy22132@nottingham.edu.cn>
2019-04-04 04:03:29 +0100

    Add more assets

--- src/MazeMap.cpp
+++ src/MazeMap.cpp
@@ -28,30 +28,52 @@ void MazeMap::virtDrawTileAt(
 	case 0: // empty ground
 		break;
 	case 1: // wall
-		pSurface->drawRectangle(
-			iStartPositionScreenX,
-			iStartPositionScreenY,
-			iStartPositionScreenX + getTileWidth() - 1,
-			iStartPositionScreenY + getTileHeight() - 1,
-			0xff0000);
+		{
+			SimpleImage image = ImageManager::loadImage("resources/images/wall.png", true).resizeTo(m_iTileWidth, m_iTileWidth);
+			image.renderImage(pSurface,
+				0,
+				0,
+				iStartPositionScreenX,
+				iStartPositionScreenY,
+				m_iTileWidth,
+				m_iTileHeight);
+		}
 		break;
-	case 2: // thron
-		pSurface->drawRectangle(
-			iStartPositionScreenX,
-			iStartPositionScreenY,
-			iStartPositionScreenX + getTileWidth() - 1,
-			iStartPositionScreenY + getTileHeight() - 1,
-			0x00ffff);
+	case 2: // thorn
+		{
+			SimpleImage image = ImageManager::loadImage("resources/images/thorn.png", true).resizeTo(m_iTileWidth, m_iTileWidth);
+			image.renderImageWithMask(pSurface,
+				0,
+				0,
+				iStartPositionScreenX,
+				iStartPositionScreenY,
+				m_iTileWidth,
+				m_iTileHeight);
+		}
 		break;
-	case 3: // door
-		pSurface->drawRectangle(
-			iStartPositionScreenX,
-			iStartPositionScreenY,
-			iStartPositionScreenX + getTileWidth() - 1,
-			iStartPositionScreenY + getTileHeight() - 1,
-			0xffffff);
+	case 3: // home
+		{
+			SimpleImage image = ImageManager::loadImage("resources/images/home.png", true).resizeTo(m_iTileWidth, m_iTileWidth);
+			image.renderImageWithMask(pSurface,
+				0,
+				0,
+				iStartPositionScreenX,
+				iStartPositionScreenY,
+				m_iTileWidth,
+				m_iTileHeight);
+		}
 		break;
 	case 4: // egg
+		{
+			SimpleImage image = ImageManager::loadImage("resources/images/egg.png", true).resizeTo(m_iTileWidth, m_iTileWidth);
+			image.renderImageWithMask(pSurface,
+				0,
+				0,
+				iStartPositionScreenX,
+				iStartPositionScreenY,
+				m_iTileWidth,
+				m_iTileHeight);
+		}
 		break;
 	default: // empty ground
 		break;
